a4d10952f8f039a1ba877eb494c6fca0c4a669cc
Abbey Buroker <abbeyburoker@Abbey-Burokers-MacBook-Pro.local>
2017-02-28 16:08:15 -0500

    header and footer round 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'static/home'
+
+  get 'static/directory'
+
+  get 'static/messageBoard'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
